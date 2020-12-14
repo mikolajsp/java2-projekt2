27cc1c9a2685ee1f2ec49bfec630f5db2d86bdde
Paweł Fijałkowski <fijalkowskipablo@gmail.com>
2020-12-14 20:01:18 +0100

    Project sceleton

---
+++ Schoolify/src/main/java/pl/pw/mini/Schoolify/SchoolifyApplication.java
@@ -0,0 +1,13 @@
+package pl.pw.mini.Schoolify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolifyApplication.class, args);
+	}
+
+}
---
+++ Schoolify/src/test/java/pl/pw/mini/Schoolify/SchoolifyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pw.mini.Schoolify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
