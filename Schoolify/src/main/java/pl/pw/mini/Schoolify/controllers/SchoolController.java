a70b4e78832b66055413560f8b7df30e3bcca207
Mikołaj Spytek <mikolajspytek@gmail.com>
2020-12-18 21:01:37 +0100

    added cross origin annotation

--- Schoolify/src/main/java/pl/pw/mini/Schoolify/controllers/SchoolController.java
+++ Schoolify/src/main/java/pl/pw/mini/Schoolify/controllers/SchoolController.java
@@ -3,6 +3,7 @@ package pl.pw.mini.Schoolify.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pl.pw.mini.Schoolify.modules.School;
 import pl.pw.mini.Schoolify.services.SchoolService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/school")
 public class SchoolController {
--- Schoolify/src/main/java/pl/pw/mini/Schoolify/controllers/SearchController.java
+++ Schoolify/src/main/java/pl/pw/mini/Schoolify/controllers/SearchController.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class SearchController{
 	@Autowired
 	SchoolService ss;
 	
-
+	@CrossOrigin
 	@GetMapping("/search")
 	public ResponseEntity<List<School>> fsearch(@RequestParam Map<String,String> allFilters){
 		String[] simple = {"town","name","type"};
